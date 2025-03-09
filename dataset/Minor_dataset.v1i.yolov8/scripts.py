99347a1eee721b6f815fd1675edde694ab2fb595
Abtinz <abtinzandi@gmail.com>
2025-03-09 19:30:38 +0330

    guard rail added

---
+++ dataset/Minor_dataset.v1i.yolov8/scripts.py
@@ -0,0 +1,50 @@
+import os
+
+def process_dataset(labels_folder_path, images_folder_path):
+    # Define indices to remove and index mapping
+    remove_indices = {'10','11'}  # Indices to remove(wall and small obstacles)
+
+    for filename in os.listdir(labels_folder_path):
+        
+        label_file_path = os.path.join(labels_folder_path, filename)
+        
+        if os.path.isfile(label_file_path) and label_file_path.endswith(".txt"):
+            with open(label_file_path, "r") as file:
+                lines = file.readlines()
+            
+            modified_lines = []
+            
+            for line in lines:
+                parts = line.strip().split()
+                if parts[0] in remove_indices: # Skip this line if the label index is in remove_indices
+                    continue
+                modified_lines.append(" ".join(parts) + "\n")
+            
+            # If no valid lines remain, delete the label file and its image
+            if not modified_lines:
+                os.remove(label_file_path)
+                print(f"Removed empty label file: {label_file_path}")
+                
+                image_file_path = os.path.join(images_folder_path, filename.replace(".txt", ".jpg"))
+                if os.path.isfile(image_file_path):
+                    os.remove(image_file_path)
+                    print(f"Removed corresponding image file: {image_file_path}")
+            else:
+                # Write the updated lines back to the label file
+                with open(label_file_path, "w") as file:
+                    file.writelines(modified_lines)
+
+process_dataset(
+    labels_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\dataset\\dataset\\train\\labels",
+    images_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\dataset\\dataset\\train\\images"
+)
+
+process_dataset(
+    labels_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\dataset\\dataset\\test\\labels",
+    images_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\dataset\\dataset\\test\\images"
+)
+
+process_dataset(
+    labels_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\dataset\\dataset\\valid\\labels",
+    images_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\dataset\\dataset\\valid\\images"
+)
---
+++ dataset/gaurdrail/scripts.py
@@ -0,0 +1,54 @@
+import os
+
+def process_dataset(labels_folder_path, images_folder_path):
+    # Define indices to remove and index mapping
+    remove_indices = {'0','2'}  # Indices to remove(path-hole)
+    index_mapping = {'1': '13'}  # Indices to change(manhole)
+
+    for filename in os.listdir(labels_folder_path):
+        
+        label_file_path = os.path.join(labels_folder_path, filename)
+        
+        if os.path.isfile(label_file_path) and label_file_path.endswith(".txt"):
+            with open(label_file_path, "r") as file:
+                lines = file.readlines()
+            
+            modified_lines = []
+            
+            for line in lines:
+                parts = line.strip().split()
+                if parts[0] in remove_indices: # Skip this line if the label index is in remove_indices
+                    continue
+                if parts[0] in index_mapping: # Update the index using the mapping
+                    parts[0] = index_mapping[parts[0]]
+                
+                modified_lines.append(" ".join(parts) + "\n")
+            
+            # If no valid lines remain, delete the label file and its image
+            if not modified_lines:
+                os.remove(label_file_path)
+                print(f"Removed empty label file: {label_file_path}")
+                
+                image_file_path = os.path.join(images_folder_path, filename.replace(".txt", ".jpg"))
+                if os.path.isfile(image_file_path):
+                    os.remove(image_file_path)
+                    print(f"Removed corresponding image file: {image_file_path}")
+            else:
+                # Write the updated lines back to the label file
+                with open(label_file_path, "w") as file:
+                    file.writelines(modified_lines)
+
+process_dataset(
+    labels_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\gaurdrail\\train\\labels",
+    images_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\gaurdrail\\train\\images"
+)
+
+process_dataset(
+    labels_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\gaurdrail\\test\\labels",
+    images_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\gaurdrail\\test\\images"
+)
+
+process_dataset(
+    labels_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\gaurdrail\\valid\\labels",
+    images_folder_path="C:\\Users\\abt\\Documents\\Real-time-obstacle-detector\\data sets\\gaurdrail\\valid\\images"
+)
